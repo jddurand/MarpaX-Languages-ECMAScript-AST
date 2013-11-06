a21277428c3b0ba69c93b7dd10cce86c14260a76
Jean-Damien Durand <jeandamiendurand@free.fr>
2013-11-06 17:17:39 +0100

    generateTemplateXslate.pl: automatic generation of template's template - prototype

--- script/generateTemplateXslate.pl
+++ script/generateTemplateXslate.pl
@@ -36,3 +36,65 @@ my $descHashp = $ecmaAst->describe();
 my $destDir = File::Spec->catdir($parentDir, 'lib', 'MarpaX', 'Languages', 'ECMAScript', 'AST', 'Grammar', $ecmaAst->grammarAlias, 'Template', 'Xslate');
 remove_tree($destDir, {verbose => 1});
 make_path($destDir, {verbose => 1});
+my $g1p = $ecmaAst->describe->{G1};
+#
+# Get all LHS
+#
+my %lhs2RuleId = ();
+map {$lhs2RuleId{$g1p->{$_}->[0]} = $_} keys %{$g1p};
+my $header = do {local $/; <DATA>};
+foreach (sort {$a <=> $b} keys %{$g1p}) {
+  my $ruleId = $_;
+  my $file = File::Spec->catfile($destDir, "$ruleId.tx");
+  print "Generating $file\n";
+  if (! open(FILE, '>', $file)) {
+    die "Cannot open $file, $!";
+  }
+  print FILE $header;
+  my $rulesp = $g1p->{$ruleId};
+  my ($lhs, @rhs) = @{$rulesp};
+  print FILE <<DESC;
+:#
+:# Rule Id $ruleId: $lhs ::= @rhs
+:# where
+DESC
+  my $i = 0;
+  my @tx = ();
+  foreach (@rhs) {
+    my $rhs = $_;
+    ++$i;
+    if (exists($lhs2RuleId{$rhs})) {
+      printf FILE ":# RHS No %2d is a G1 rule %-4d: %s\n", $i, $lhs2RuleId{$rhs}, $rhs;
+      push(@tx, sprintf('$tx.render("%d.tx", $ast[%d])', $lhs2RuleId{$rhs}, $i - 1));
+    } else {
+      printf FILE ":# RHS No %2d is a lexeme      : %s\n", $i, $rhs;
+      push(@tx, sprintf('$ast[%d][2]', $i - 1));
+    }
+  }
+  print FILE <<BLOCK;
+:block G$ruleId -> {
+: @tx
+:}
+BLOCK
+  if (! close(FILE)) {
+    warn "Cannot close $file, $!";
+  }
+}
+__DATA__
+:# --------------------------------------------------
+:# This is generated template of Xslate template
+:#
+:# Default behaviour is:
+:# - call another block when an RHS is a G1 rule
+:# - output the lexeme as-is if not
+:#
+:# The caller should "cascade" the template if needed
+:# --------------------------------------------------
+:# Template input:
+:# $tx   : Xslate template object
+:# $ast  : Current ast (blessed object)
+:#
+:# Injected functions:
+:# blessed($ast) : returns the bless name
+:# data($ast)    : returns the data (array reference)
+:# --------------------------------------------------
