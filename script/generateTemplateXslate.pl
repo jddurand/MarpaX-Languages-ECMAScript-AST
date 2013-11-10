2b994f54c19bd62fb35fc3413fe34992d7ec6223
Jean-Damien Durand <jeandamiendurand@free.fr>
2013-11-10 08:50:39 +0100

    script/generateTemplateXslate: PP version

--- script/generateTemplateXslate.pl
+++ script/generateTemplateXslate.pl
@@ -2,22 +2,23 @@
 use strict;
 use warnings FATAL => 'all';
 use MarpaX::Languages::ECMAScript::AST;
-use File::Path qw/make_path remove_tree/;
 use File::Spec;
 use File::Basename qw/dirname/;
 use Cwd qw/abs_path cwd/;
+use Carp qw/croak/;
+use POSIX qw/EXIT_SUCCESS/;
 
 # -------------------------------
 # Generation of grammar templates
 # -------------------------------
-
+my @COPYARGV = @ARGV;
 my $grammarName = shift || '';
 if (! $grammarName) {
   die "Usage: $^X $0 grammarName";
 }
 
 #
-# We generate Xslate templates based on the G1 grammar. Note that we MUST be
+# We generate templates as a perl class based on the G1 grammar. Note that we MUST be
 # in the directory just upper than script/ . We use Cwd and $0 and verify that
 # Cwd is the dirname of the dirname of $0.
 #
@@ -33,69 +34,156 @@ if (! $isSameDir) {
 
 my $ecmaAst = MarpaX::Languages::ECMAScript::AST->new(grammarName => $grammarName);
 my $descHashp = $ecmaAst->describe();
-my $destDir = File::Spec->catdir($parentDir, 'lib', 'MarpaX', 'Languages', 'ECMAScript', 'AST', 'Grammar', $ecmaAst->grammarAlias, 'Template', 'Xslate');
-remove_tree($destDir, {verbose => 1});
-make_path($destDir, {verbose => 1});
 my $g1p = $ecmaAst->describe->{G1};
 #
 # Get all LHS
 #
-my %lhs2RuleId = ();
-map {$lhs2RuleId{$g1p->{$_}->[0]} = $_} keys %{$g1p};
-my $header = do {local $/; <DATA>};
-foreach (sort {$a <=> $b} keys %{$g1p}) {
-  my $ruleId = $_;
-  my $file = File::Spec->catfile($destDir, "$ruleId.tx");
-  print "Generating $file\n";
-  if (! open(FILE, '>', $file)) {
+my %lhs = ();
+map {$lhs{$g1p->{$_}->[0]}++} keys %{$g1p};
+#
+# Generate Template.pm
+#
+my $grammarAlias = $ecmaAst->grammarAlias;
+my $file = File::Spec->catfile($parentDir, 'lib', 'MarpaX', 'Languages', 'ECMAScript', 'AST', 'Grammar', $grammarAlias, 'Template.pm');
+if (! open(FILE, '>', $file)) {
     die "Cannot open $file, $!";
-  }
-  print FILE $header;
-  my $rulesp = $g1p->{$ruleId};
-  my ($lhs, @rhs) = @{$rulesp};
-  print FILE <<DESC;
-:#
-:# Rule Id $ruleId: $lhs ::= @rhs
-:# where
-DESC
-  my $i = 0;
-  my @tx = ();
-  foreach (@rhs) {
-    my $rhs = $_;
-    ++$i;
-    if (exists($lhs2RuleId{$rhs})) {
-      printf FILE ":# RHS No %2d is G1 rule %-4d: %s\n", $i, $lhs2RuleId{$rhs}, $rhs;
-      push(@tx, sprintf('$tx.render("%d.tx", $ast[%d])', $lhs2RuleId{$rhs}, $i - 1));
-    } else {
-      printf FILE ":# RHS No %2d is a lexeme    : %s\n", $i, $rhs;
-      push(@tx, sprintf('$ast[%d][2]', $i - 1));
-    }
-  }
-  my $tx = join(' ~ ', @tx);
-  print FILE <<BLOCK;
-:block G$ruleId -> {
-: $tx
-:}
-BLOCK
-  if (! close(FILE)) {
-    warn "Cannot close $file, $!";
-  }
 }
+#
+# We search for the LHS '[:start]'. The real starting point
+# will be its LHS
+#
+my $startRuleId = undef;
+foreach (keys %{$g1p}) {
+    my $ruleId = $_;
+    my $rulesp = $g1p->{$ruleId};
+    my ($lhs, @rhs) = @{$rulesp};
+    if ($lhs eq '[:start]') {
+	$startRuleId = $ruleId;
+    }
+}
+if (! defined($startRuleId)) {
+    croak 'Cannot find :start';
+}
+
+print "Generating $file\n";
+print FILE <<HEADER;
+#
+# This is a generated file using the command:
+# $^X $0 @COPYARGV
+#
+use strict;
+use warnings FATAL => 'all';
+no warnings 'recursion';
+
+package MarpaX::Languages::ECMAScript::AST::Grammar::${grammarAlias}::Template;
+HEADER
+#
+# The fixed stuff: new(), lexeme(), indent(), transpile()
+#
+print FILE do {local $/; <DATA>};
+foreach (sort {$a <=> $b} keys %{$g1p}) {
+    my $ruleId = $_;
+    my $rulesp = $g1p->{$ruleId};
+    my ($lhs, @rhs) = @{$rulesp};
+    print FILE "
+#
+# ---------------------------------------------------
+# $lhs ::= @rhs
+# ---------------------------------------------------
+sub G1_$ruleId {
+    my (\$self, \$value, \$index) = \@_;
+
+    my \$rc = '';
+
+";
+    foreach (0..$#rhs) {
+        printf FILE "    %sif (\$index == $_) {\n", $_ > 0 ? 'els' : '';
+	if (exists($lhs{$rhs[$_]})) {
+            #print FILE "    my \$method$_ = defined(\$value->[$_]) ? \"G1_\$value->[$_]->{ruleId}\" : undef;\n";
+            #print FILE "    my \$value$_ = (defined(\$method$_) ? (\$indent . \$self->\$method$_(\$value->[$_]->{values})) : '');\n";
+	    #push(@value, "\$value$_");
+	} else {
+            print FILE "        \$rc = \$self->lexeme(\$value);\n";
+	}
+        print FILE "    }\n";
+    }
+    print FILE "\n";
+    print FILE "    return \$rc;\n";
+    print FILE "}\n";
+}
+print FILE "\n1;\n";
+
+if (! close(FILE)) {
+    warn "Cannot close $file, $!";
+}
+exit(EXIT_SUCCESS);
+
 __DATA__
-:# --------------------------------------------------
-:# This is generated template of Xslate template
-:#
-:# Default behaviour is:
-:# - call another block when an RHS is a G1 rule
-:# - output the lexeme as-is if not
-:#
-:# The caller should "cascade" the template if needed
-:# --------------------------------------------------
-:# Template input:
-:# $tx   : Xslate template object
-:# $ast  : Current ast (blessed object)
-:#
-:# Injected functions:
-:# blessed($ast) : returns the bless name
-:# data($ast)    : returns the data (array reference)
-:# --------------------------------------------------
+
+# ABSTRACT: Template for ${grammarAlias} transpilation using an AST
+
+# VERSION
+
+sub new {
+    my ($class, %options) = @_;
+
+    my $self = {_nindent => 0};
+    bless($self, $class);
+    return $self;
+}
+
+sub lexeme {
+    my ($self, $value) = @_;
+
+    my $lexeme = $value->[2];
+    if    ($lexeme eq ';') { $lexeme .= "\n" . $self->indent();  }
+    elsif ($lexeme eq '{') { $lexeme .= "\n" . $self->indent(1); }
+    elsif ($lexeme eq '}') { $lexeme = "\n" . $self->indent(-1) . ' ' . $lexeme;}
+
+    return ' ' . $lexeme;
+}
+
+sub indent {
+    my ($self, $inc) = @_;
+
+    if (defined($inc)) {
+	$self->{_nindent} += $inc;
+    }
+
+    return '  ' x $self->{_nindent};
+}
+
+sub transpile {
+    my ($self, $ast) = @_;
+
+    my @worklist = ($ast);
+    my $transpile = '';
+    do {
+	my $obj = shift(@worklist);
+	if (ref($obj) eq 'HASH') {
+	    my $g1 = 'G1_' . $obj->{ruleId};
+	    # print STDERR "==> @{$obj->{values}}\n";
+	    foreach (reverse 0..$#{$obj->{values}}) {
+		my $value = $obj->{values}->[$_];
+		if (ref($value) eq 'HASH') {
+		    # print STDERR "Unshift $value\n";
+		    unshift(@worklist, $value);
+		} else {
+		    # print STDERR "Unshift [ $g1, $value, $_ ]\n";
+		    unshift(@worklist, [ $g1, $value, $_ ]);
+		}
+	    }
+	} else {
+	    my ($curMethod, $value, $index) = @{$obj};
+	    # print STDERR "==> Calling $curMethod($value, $index)\n";
+	    $transpile .= $self->$curMethod($value, $index);
+	    # print STDERR "==> $transpile\n";
+	}
+    } while (@worklist);
+
+    return $transpile;
+
+#    my ($ruleId, $value) = ($ast->{ruleId}, $ast->{values});
+#    my $method = "G1_$ruleId";
+#    return $self->$method($value);
+}
