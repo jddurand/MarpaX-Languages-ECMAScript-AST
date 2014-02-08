e2bef4affe67a31dc1ddc0219c1576ff9b4328eb
Jean-Damien Durand <jeandamiendurand@free.fr>
2014-02-08 19:03:36 +0100

    StringNumericLiteral/NativeNumberSemantics.pm: Try to deal with platforms without pos_zero or pos_infinity

--- lib/MarpaX/Languages/ECMAScript/AST/Grammar/ECMAScript_262_5/StringNumericLiteral/NativeNumberSemantics.pm
+++ lib/MarpaX/Languages/ECMAScript/AST/Grammar/ECMAScript_262_5/StringNumericLiteral/NativeNumberSemantics.pm
@@ -2,16 +2,28 @@ use strict;
 use warnings FATAL => 'all';
 
 package MarpaX::Languages::ECMAScript::AST::Grammar::ECMAScript_262_5::StringNumericLiteral::NativeNumberSemantics;
-use Data::Float qw//;
+use Data::Float qw/have_signed_zero have_infinite/;
 use Scalar::Util qw/blessed/;
+use Scalar::Util::Numeric qw/isinf/;
 
-# ABSTRACT: ECMAScript 262, Edition 5, lexical string numeric grammar default semantics package, using native perl number representations
+our $POS_ZERO = have_signed_zero() ? Data::Float::pos_zero() : 0.0;
+#
+# C.f. http://computer-programming-forum.com/53-perl/3bbc35c5223c25f7.htm
+# On some machine this may give 1.79769313486232e+308 (Alpha/HPUX), i.e.
+# when floats on are 64bits when ints are on 32bits
+#
+our $POS_INF_CANDIDATE1 = (~0)**(~0);
+our $POS_INF_CANDIDATE2 = do {my $n = 2; $n *= $n while $n < $n*$n; $n};
+
+our $POS_INF  = have_infinite() ? Data::Float::pos_infinity() : (isinf($POS_INF_CANDIDATE1) ? $POS_INF_CANDIDATE1 : $POS_INF_CANDIDATE2);
+
+# ABSTRACT: ECMAScript 262, Edition 5, lexical string numeric grammar default semantics package, using native perl representations
 
 # VERSION
 
 =head1 DESCRIPTION
 
-This modules provide a default semantics package for the actions associated to ECMAScript_262_5 lexical string numeric grammar.
+This modules provide a default semantics package for the actions associated to ECMAScript_262_5 lexical string numeric grammar, using native number representation. This mean that the notion of Infinity, or positive zero, might not exactly follow ECMAScript specification (which restrict numbers to IEEE 32 bits floats).
 
 =cut
 
@@ -48,23 +60,23 @@ sub host_round {
 
 =head2 host_pos_zero($self)
 
-Host implementation of $self setted to positive zero, defaulting to Data::Float::pos_zero(). Returns $self.
+Host implementation of $self setted to positive zero, defaulting to Data::Float::pos_zero if your host have signed zeroes, 0 otherwise. Returns $self.
 
 =cut
 
 sub host_pos_zero {
-    $_[0]->{_number} = Data::Float::pos_zero();
+    $_[0]->{_number} = $POS_ZERO;
     return $_[0];
 }
 
 =head2 host_pos_inf($self)
 
-Host implementation of $self setted to positive infinity, defaulting to Data::Float::pos_infinity(). Return $self.
+Host implementation of $self setted to positive infinity, defaulting to Data::Float::pos_infinity if your host have infinity, otherwise a trial between (~0)**(~0) and {my $n = 2; $n *= $n while $n < $n*$n; $n}. Return $self.
 
 =cut
 
 sub host_pos_inf {
-    $_[0]->{_number} = Data::Float::pos_infinity();
+    $_[0]->{_number} = $POS_INF;
     return $_[0];
 }
 
