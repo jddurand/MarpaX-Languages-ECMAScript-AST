d79c80b6b532f00f8c918968eddaae3f71603d73
Jean-Damien Durand <jeandamiendurand@free.fr>
2014-07-23 21:28:08 +0200

    ECMAScript_262_5 Generic Template for version 0.018

--- lib/MarpaX/Languages/ECMAScript/AST/Grammar/ECMAScript_262_5/Template.pm
+++ lib/MarpaX/Languages/ECMAScript/AST/Grammar/ECMAScript_262_5/Template.pm
@@ -1,6 +1,6 @@
 #
 # This is a generated file using the command:
-# C:\perl-5.18\perl\bin\perl.exe script/generateTemplate.pl ECMAScript-262-5
+# /usr/bin/perl script/generateTemplate.pl ECMAScript-262-5
 #
 use strict;
 use warnings FATAL => 'all';
